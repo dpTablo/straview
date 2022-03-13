5f9719a4c48318549e9d533bb4a8bb71afa9f554
dpTablo <leeyw2355@gmail.com>
2022-03-13 10:31:20 +0900

    ResourceState enum 변경

---
+++ src/main/java/com/dptablo/straview/dto/enumtype/ResourceState.java
@@ -0,0 +1,37 @@
+package com.dptablo.straview.dto.enumtype;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+public enum ResourceState {
+    META(1),
+    SUMMARY(2),
+    DETAIL(3);
+
+    @Getter
+    private final Integer value;
+
+    ResourceState(Integer value) {
+        this.value = value;
+    }
+
+    @JsonCreator
+    public static ResourceState valueOf(Integer value) {
+        switch (value) {
+            case 1:
+                return ResourceState.META;
+            case 2:
+                return ResourceState.SUMMARY;
+            case 3:
+                return ResourceState.DETAIL;
+            default:
+                throw new IllegalArgumentException("ResourceState argument : " + value);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
---
+++ src/main/java/com/dptablo/straview/dto/enumtype/converter/ResourceStateConverter.java
@@ -0,0 +1,17 @@
+package com.dptablo.straview.dto.enumtype.converter;
+
+import com.dptablo.straview.dto.enumtype.ResourceState;
+
+import javax.persistence.AttributeConverter;
+
+public class ResourceStateConverter implements AttributeConverter<ResourceState, Integer> {
+    @Override
+    public Integer convertToDatabaseColumn(ResourceState attribute) {
+        return attribute.getValue();
+    }
+
+    @Override
+    public ResourceState convertToEntityAttribute(Integer dbData) {
+        return ResourceState.valueOf(dbData);
+    }
+}
