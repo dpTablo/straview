88692d8c116dabfa654f442d4ce2810f2f65d443
dpTablo <leeyw2355@gmail.com>
2022-03-16 13:36:33 +0900

    코드 리팩토링 -미사용 코드 제거

--- src/main/java/com/dptablo/straview/repository/ActivityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dptablo.straview.repository;
-
-import com.dptablo.straview.dto.entity.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActivityRepository extends JpaRepository<Activity, Integer> {
-}
