a0fb00ec11368ad14f98a56a4ccddc92d5e2c176
dpTablo <leeyw2355@gmail.com>
2022-03-21 02:17:56 +0900

    Strava 동기화 서비스 -동기화 시간 정보 처리 구현 -선수정보, 기어정보, 액티비티 목록, 동기화 시간 정보에 대한 로직 구현
    Strava 동기화 컨트롤러
    -리턴타입을 동기화 시간 정보로 변경

--- src/main/java/com/dptablo/straview/controller/rest/StravaSynchronizeController.java
+++ src/main/java/com/dptablo/straview/controller/rest/StravaSynchronizeController.java
@@ -1,5 +1,6 @@
 package com.dptablo.straview.controller.rest;
 
+import com.dptablo.straview.dto.entity.StravaSyncInfo;
 import com.dptablo.straview.exception.StraviewException;
 import com.dptablo.straview.service.StravaSynchronizeService;
 import lombok.RequiredArgsConstructor;
@@ -10,9 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/v1/strava")
 @RequiredArgsConstructor
@@ -21,16 +19,13 @@ public class StravaSynchronizeController {
     private final StravaSynchronizeService stravaSynchronizeService;
 
     @GetMapping("/synchronize")
-    public ResponseEntity<Map<String, Object>> synchronize() {
-        //
-        HashMap<String, Object> resultMap = new HashMap<>();
+    public ResponseEntity<StravaSyncInfo> synchronize() {
         try {
-            boolean isSynchronized = stravaSynchronizeService.synchronize();
-            return new ResponseEntity(resultMap, HttpStatus.OK);
+            StravaSyncInfo syncInfo = stravaSynchronizeService.synchronize();
+            return new ResponseEntity(syncInfo, HttpStatus.OK);
         } catch(StraviewException e) {
             log.error(e.getMessage());
-            resultMap.put("message", e.getErrorCode().getDescription());
-            return new ResponseEntity(resultMap, e.getErrorCode().getHttpStatus());
+            return new ResponseEntity(null, e.getErrorCode().getHttpStatus());
         }
     }
 }
--- src/main/java/com/dptablo/straview/service/StravaSynchronizeService.java
+++ src/main/java/com/dptablo/straview/service/StravaSynchronizeService.java
@@ -1,41 +1,82 @@
 package com.dptablo.straview.service;
 
 import com.dptablo.straview.ApplicationProperty;
+import com.dptablo.straview.common.OptionalConsumer;
+import com.dptablo.straview.dto.entity.Gear;
 import com.dptablo.straview.dto.entity.StravaAthlete;
 import com.dptablo.straview.dto.entity.StravaSyncInfo;
+import com.dptablo.straview.dto.entity.SummaryActivity;
+import com.dptablo.straview.exception.AuthenticationException;
 import com.dptablo.straview.exception.StraviewException;
+import com.dptablo.straview.repository.GearRepository;
+import com.dptablo.straview.repository.StravaAthleteRepository;
 import com.dptablo.straview.repository.StravaSyncInfoRepository;
+import com.dptablo.straview.repository.SummaryActivityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class StravaSynchronizeService {
+    public static final int PER_PAGE = 100;
+
     private final StravaSyncInfoRepository syncInfoRepository;
+    private final StravaSummaryActivityService stravaSummaryActivityService;
     private final StravaAthleteService athleteService;
+    private final StravaAthleteRepository athleteRepository;
+    private final GearRepository gearRepository;
+    private final SummaryActivityRepository summaryActivityRepository;
     private final ApplicationProperty applicationProperty;
 
-    @Transactional
-    public boolean synchronize() throws StraviewException {
-        Long athleteId = applicationProperty.getStravaClientAthleteId();
+    /**
+     * <p>Strava 의 데이터를 요청하여 Straview DB에 동기화 합니다.</p>
+     *
+     * @return 동기화 정보
+     * @throws StraviewException 동기화 프로세스 과정의 에러
+     */
+    @Transactional(rollbackFor = StraviewException.class)
+    public StravaSyncInfo synchronize() throws StraviewException {
+        StravaSyncInfo syncInfo = getSyncInfo();
 
-        StravaSyncInfo syncInfo = syncInfoRepository.findById(athleteId)
+        processingAthleteAndGears();
+        processingActivities(syncInfo);
+        return processingSyncInfo(syncInfo);
+    }
+
+    private void processingActivities(StravaSyncInfo syncInfo) throws StraviewException {
+        List<SummaryActivity> activities = stravaSummaryActivityService.getLoggedInAthleteActivities(syncInfo.getSyncEpochTime(), PER_PAGE);
+        summaryActivityRepository.saveAll(activities);
+    }
+
+    private StravaSyncInfo getSyncInfo() {
+        Long athleteId = applicationProperty.getStravaClientAthleteId();
+        return syncInfoRepository.findById(athleteId)
                 .orElse(StravaSyncInfo.builder()
                         .athleteId(athleteId)
+                        .syncEpochTime(0L)
                         .build());
-        syncInfo.setSyncEpochTime(Instant.now().getEpochSecond());
-        syncInfoRepository.save(syncInfo);
+    }
 
-        //TODO athlete 동기화
+    private StravaSyncInfo processingSyncInfo(StravaSyncInfo syncInfo) {
+        syncInfo.setSyncEpochTime(Instant.now().getEpochSecond());
+        return syncInfoRepository.save(syncInfo);
+    }
+
+    private void processingAthleteAndGears() throws AuthenticationException {
         StravaAthlete athlete = athleteService.getLoggedInAthlete();
 
-        //TODO gear 동기화
+        //athlete 정보의 bike 에 대한 기존 데이터 pk 매핑
+        List<Gear> bikes = athlete.getBikes();
+        for(Gear bike : bikes) {
+            OptionalConsumer.of(
+                    gearRepository.findByGearIdAndAthleteId(bike.getGearId(), bike.getAthlete().getAthleteId())
+            ).ifPresent(foundGear -> bike.setManageId(foundGear.getManageId()));
+        }
 
-        //TODO activity 동기화
-
-        return false;
+        athleteRepository.save(athlete);
     }
 }
